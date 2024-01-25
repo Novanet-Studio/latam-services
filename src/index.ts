eacde6f36bf2fda8a6a227c73a93f1484e48dea7
David Arenas <davejs136@gmail.com>
2024-01-25 00:06:01 -0400

    fix: no set content type for POST requests

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,60 @@
 import { Elysia, t } from "elysia";
-import { cors } from '@elysiajs/cors'
+import { cors } from "@elysiajs/cors";
 
 const PORT = Number(process.env.PORT) || 3001;
 const userApiKey = process.env.USERS_API_KEY;
 const userApiUrl = process.env.USERS_API_URL;
 const btApiUrl = process.env.BT_BASE_API_URL;
 
-const bussinessRif = process.env.BUSSINESS_RIF
-const bussinessName = process.env.BUSSINESS_NAME
-const bussinessAfiliatedCode = process.env.AFILIATED_CODE
+const bussinessRif = process.env.BUSSINESS_RIF;
+const bussinessName = process.env.BUSSINESS_NAME;
+const bussinessAfiliatedCode = process.env.AFILIATED_CODE;
 
-const app = new Elysia({ prefix: '/api' });
+const app = new Elysia({ prefix: "/api" });
 
-app.use(cors())
+app.use(cors());
 
-app.post('/get-client-details', async ({ body, set }) => {
-  try {
-    const response = await fetch(`${userApiUrl}/GetClientsDetails`, {
-      method: 'POST',
-      body: JSON.stringify({
-        token: userApiKey,
-        cedula: body.cedula,
-      })
-    });
+app.post(
+  "/get-client-details",
+  async ({ body, set }) => {
+    try {
+      const response = await fetch(`${userApiUrl}/GetClientsDetails`, {
+        method: "POST",
+        body: JSON.stringify({
+          token: userApiKey,
+          cedula: body.cedula,
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
 
-    const json = await response.json();
+      const json = await response.json();
 
-    return json;
-  } catch (error) {
-    set.status = "Internal Server Error"
+      return json;
+    } catch (error) {
+      set.status = "Internal Server Error";
 
-    return {
-      status: "Internal Server Error",
-      error: "Error interno de servidor"
+      return {
+        status: "Internal Server Error",
+        error: "Error interno de servidor",
+      };
     }
+  },
+  {
+    body: t.Object({
+      cedula: t.String(),
+    }),
   }
-}, {
-  body: t.Object({
-    cedula: t.String(),
-  })
-});
+);
 
-app.post('/bancos', async ({ set }) => {
+app.post("/bancos", async ({ set }) => {
   try {
     const response = await fetch(`${btApiUrl}/bancos`, {
-      method: 'POST'
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
     const json = await response.json();
@@ -55,113 +65,134 @@ app.post('/bancos', async ({ set }) => {
 
     return {
       status: "Internal Server Error",
-      error: "Error interno de servidor"
-    }
+      error: "Error interno de servidor",
+    };
   }
 });
 
-app.post('/clave', async ({ body, set }) => {
-  try {
-    const response = await fetch(`${btApiUrl}/lotes/solicitud/clave`, {
-      method: 'POST',
-      body: JSON.stringify({
-        canal: '01',
-        celularDestino: body.celularDestino
-      })
-    });
+app.post(
+  "/clave",
+  async ({ body, set }) => {
+    try {
+      const response = await fetch(`${btApiUrl}/lotes/solicitud/clave`, {
+        method: "POST",
+        body: JSON.stringify({
+          canal: "01",
+          celularDestino: body.celularDestino,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    const json = await response.json();
+      const json = await response.json();
 
-    return json;
-  } catch (error) {
-    set.status = "Internal Server Error";
+      return json;
+    } catch (error) {
+      set.status = "Internal Server Error";
 
-    return {
-      status: "Internal Server Error",
-      error: "Error interno de servidor"
+      return {
+        status: "Internal Server Error",
+        error: "Error interno de servidor",
+      };
     }
+  },
+  {
+    body: t.Object({
+      celularDestino: t.String(),
+    }),
   }
-}, {
-  body: t.Object({
-    celularDestino: t.String(),
-  })
-});
+);
 
-app.post('/pago', async ({ body, set }) => {
-  try {
-    const response = await fetch(`${btApiUrl}/botonDePago/pago`, {
-      method: 'POST',
-      body: JSON.stringify({
-        canal: '06',
-        celular: body.celular,
-        banco: body.banco,
-        RIF: bussinessRif,
-        cedula: body.cedula,
-        monto: body.monto,
-        token: body.token,
-        concepto: `Pago de servicios de ${body.nombre}`,
-        codAfiliado: bussinessAfiliatedCode,
-        comercio: bussinessName,
-      })
-    });
+app.post(
+  "/pago",
+  async ({ body, set }) => {
+    try {
+      const response = await fetch(`${btApiUrl}/botonDePago/pago`, {
+        method: "POST",
+        body: JSON.stringify({
+          canal: "06",
+          celular: body.celular,
+          banco: body.banco,
+          RIF: bussinessRif,
+          cedula: body.cedula,
+          monto: body.monto,
+          token: body.token,
+          concepto: `Pago de servicios de ${body.nombre}`,
+          codAfiliado: bussinessAfiliatedCode,
+          comercio: bussinessName,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    const json = await response.json();
+      const json = await response.json();
 
-    return json;
-  } catch (error) {
-    set.status = "Internal Server Error";
+      return json;
+    } catch (error) {
+      set.status = "Internal Server Error";
 
-    return {
-      status: "Internal Server Error",
-      error: "Error interno de servidor"
+      return {
+        status: "Internal Server Error",
+        error: "Error interno de servidor",
+      };
     }
+  },
+  {
+    body: t.Object({
+      celular: t.String(),
+      banco: t.String(),
+      cedula: t.String(),
+      monto: t.String(),
+      token: t.String(),
+      nombre: t.String(),
+    }),
   }
-}, {
-  body: t.Object({
-    celular: t.String(),
-    banco: t.String(),
-    cedula: t.String(),
-    monto: t.String(),
-    token: t.String(),
-    nombre: t.String(),
-  })
-});
+);
 
-app.post('/conformacion', async ({ body, set }) => {
-  try {
-    const response = await fetch(`${btApiUrl}/botonDePago/conformacion`, {
-      method: 'POST',
-      body: JSON.stringify({
-        referencia: body.referencia,
-        monto: body.monto,
-        banco: body.banco,
-        codAfiliado: bussinessAfiliatedCode,
-        fecha: body.fecha,
-        celular: body.celular,
-        RIF: bussinessRif,
-      })
-    });
+app.post(
+  "/conformacion",
+  async ({ body, set }) => {
+    try {
+      const response = await fetch(`${btApiUrl}/botonDePago/conformacion`, {
+        method: "POST",
+        body: JSON.stringify({
+          referencia: body.referencia,
+          monto: body.monto,
+          banco: body.banco,
+          codAfiliado: bussinessAfiliatedCode,
+          fecha: body.fecha,
+          celular: body.celular,
+          RIF: bussinessRif,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    const json = await response.json();
+      const json = await response.json();
 
-    return json;
-  } catch (error) {
-    set.status = "Internal Server Error";
+      return json;
+    } catch (error) {
+      set.status = "Internal Server Error";
 
-    return {
-      status: "Internal Server Error",
-      error: "Error interno de servidor"
+      return {
+        status: "Internal Server Error",
+        error: "Error interno de servidor",
+      };
     }
+  },
+  {
+    body: t.Object({
+      referencia: t.String(),
+      monto: t.String(),
+      banco: t.String(),
+      fecha: t.String(),
+      celular: t.String(),
+    }),
   }
-}, {
-  body: t.Object({
-    referencia: t.String(),
-    monto: t.String(),
-    banco: t.String(),
-    fecha: t.String(),
-    celular: t.String(),
-  })
-});
+);
 
 app.listen(PORT);
 
