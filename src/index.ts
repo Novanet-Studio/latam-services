ee97f2cf6fd00c2da261d3783ec5bd834cb982fb
David Arenas <davejs136@gmail.com>
2024-10-11 19:22:30 -0400

    feat: add logger

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import { Elysia, t } from "elysia";
+import { logger } from "@chneau/elysia-logger";
 import { cors } from "@elysiajs/cors";
 import routes from "./routes";
 
 const PORT = Number(process.env.PORT) || 8001;
 
-const app = new Elysia({ prefix: "/api" })
-  .use(routes)
-  .use(cors())
-  .listen(PORT);
+const app = new Elysia({ prefix: "/api" });
+
+app.use(cors());
+app.use(logger());
+
+// Routes
+app.use(routes);
+
+// Run server
+app.listen(PORT);
 
 console.log(
   `🦊 LATAM services is running at ${app.server?.hostname}:${app.server?.port}`
