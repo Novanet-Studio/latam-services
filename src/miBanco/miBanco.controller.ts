4c6157697b08ba06ddcfc4a7d74782ee78426eed
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2025-01-20 13:13:05 -0400

    fix: remove static response

--- src/miBanco/miBanco.controller.ts
+++ src/miBanco/miBanco.controller.ts
@@ -79,10 +79,83 @@ export async function notifyHandler({ store, body, set }: Params) {
 }
 
 export async function emitSSEHandler({ store, set }: Params) {
+  /*
+  store = {
+    canNotify: true,
+    data: {
+      CstmrPmtStsRpt: {
+        GrpHdr: {
+          MsgId: "BTCB012025011715155111853606",
+          CreDtTm: "2025-01-17T15:15:51",
+          InitgPty: { Id: { PrvtId: { Othr: { Id: "0071" } } } },
+        },
+        OrgnlGrpInfAndSts: {
+          OrgnlMsgId: "0001012025011719154400000000",
+          OrgnlCreDtTm: "2025-01-17T19:15:44Z",
+          OrgnlNbOfTxs: 1,
+          OrgnlCtrlSum: 5.47,
+          GrpSts: "ACCP",
+        },
+        OrgnlPmtInfAndSts: [
+          {
+            TxInfAndSts: [
+              {
+                OrgnlInstrId: "0591b937-ff02-47be-a05b-4a670682cd05",
+                OrgnlEndToEndId: "81692025011715154611853576",
+                TxSts: "ACCP",
+                OrgnlTxRef: {
+                  InstdAmt: { Amt: 5.47, Ccy: "VES" },
+                  IntrBkSttlmDt: "2025-01-17",
+                  IntrBkSttlmNb: 1,
+                  PmtTpInf: { LclInstrm: { Cd: "050" } },
+                  Dbtr: {
+                    Nm: "Alexander ",
+                    Id: {
+                      PrvtId: {
+                        Othr: { Id: "V3413756", SchmeNm: { Cd: "SCID" } },
+                      },
+                    },
+                  },
+                  DbtrAcct: { Prxy: { Tp: { Cd: "CELE" }, Id: "04242785127" } },
+                  DbtrAgt: {
+                    FinInstnId: {
+                      ClrSysMmbId: { ClrSysId: { Cd: "NCCE" }, MmbId: "0169" },
+                    },
+                  },
+                  CdtrAgt: {
+                    FinInstnId: {
+                      ClrSysMmbId: { ClrSysId: { Cd: "NCCE" }, MmbId: "0169" },
+                    },
+                  },
+                  Cdtr: {
+                    Nm: "Latin American Cable",
+                    Id: {
+                      PrvtId: {
+                        Othr: { Id: "J298946229", SchmeNm: { Cd: "SRIF" } },
+                      },
+                    },
+                  },
+                  CdtrAcct: {
+                    Prxy: { Tp: { Cd: "CNTA" }, Id: "01690001041000579342" },
+                  },
+                  Purp: { Cd: "002" },
+                  RmtInf: { Ustrd: "DESCRIPCION DEL COBRO" },
+                },
+              },
+            ],
+          },
+        ],
+      },
+    },
+  };
+  */
+
   try {
     logger.info(store?.canNotify, "emit_sse_can_notify");
 
     if (store?.canNotify) {
+      logger.info("<<< CLOSE notifyHandler");
+
       const response = new Stream((stream) => {
         const interval = setInterval(() => {
           stream.send(JSON.stringify(store!.data));
