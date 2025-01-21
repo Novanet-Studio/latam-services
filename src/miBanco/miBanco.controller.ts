bc9cd9474977cfdfbaaf2692316c9c89548f43ea
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2025-01-21 09:46:44 -0400

    add: logs of process

--- src/miBanco/miBanco.controller.ts
+++ src/miBanco/miBanco.controller.ts
@@ -14,6 +14,8 @@ interface Params {
 const logger = new Logger("miBanco");
 
 export async function requestOTPHandler({ body, set }: Params) {
+  console.log(`<<< ON requestOTPHandler (body)`, JSON.stringify(body));
+
   try {
     const response = await requestOTP(body);
     const json = await response.json();
@@ -49,7 +51,7 @@ export async function makePaymentHandler({ body, set }: Params) {
 }
 
 export async function notifyHandler({ store, body, set }: Params) {
-  logger.info("<<< START notifyHandler");
+  console.log(`<<< ON notifyHandler (store)`, JSON.stringify(store));
 
   try {
     if (!store) {
@@ -79,83 +81,12 @@ export async function notifyHandler({ store, body, set }: Params) {
 }
 
 export async function emitSSEHandler({ store, set }: Params) {
-  /*
-  store = {
-    canNotify: true,
-    data: {
-      CstmrPmtStsRpt: {
-        GrpHdr: {
-          MsgId: "BTCB012025011715155111853606",
-          CreDtTm: "2025-01-17T15:15:51",
-          InitgPty: { Id: { PrvtId: { Othr: { Id: "0071" } } } },
-        },
-        OrgnlGrpInfAndSts: {
-          OrgnlMsgId: "0001012025011719154400000000",
-          OrgnlCreDtTm: "2025-01-17T19:15:44Z",
-          OrgnlNbOfTxs: 1,
-          OrgnlCtrlSum: 5.47,
-          GrpSts: "ACCP",
-        },
-        OrgnlPmtInfAndSts: [
-          {
-            TxInfAndSts: [
-              {
-                OrgnlInstrId: "0591b937-ff02-47be-a05b-4a670682cd05",
-                OrgnlEndToEndId: "81692025011715154611853576",
-                TxSts: "ACCP",
-                OrgnlTxRef: {
-                  InstdAmt: { Amt: 5.47, Ccy: "VES" },
-                  IntrBkSttlmDt: "2025-01-17",
-                  IntrBkSttlmNb: 1,
-                  PmtTpInf: { LclInstrm: { Cd: "050" } },
-                  Dbtr: {
-                    Nm: "Alexander ",
-                    Id: {
-                      PrvtId: {
-                        Othr: { Id: "V3413756", SchmeNm: { Cd: "SCID" } },
-                      },
-                    },
-                  },
-                  DbtrAcct: { Prxy: { Tp: { Cd: "CELE" }, Id: "04242785127" } },
-                  DbtrAgt: {
-                    FinInstnId: {
-                      ClrSysMmbId: { ClrSysId: { Cd: "NCCE" }, MmbId: "0169" },
-                    },
-                  },
-                  CdtrAgt: {
-                    FinInstnId: {
-                      ClrSysMmbId: { ClrSysId: { Cd: "NCCE" }, MmbId: "0169" },
-                    },
-                  },
-                  Cdtr: {
-                    Nm: "Latin American Cable",
-                    Id: {
-                      PrvtId: {
-                        Othr: { Id: "J298946229", SchmeNm: { Cd: "SRIF" } },
-                      },
-                    },
-                  },
-                  CdtrAcct: {
-                    Prxy: { Tp: { Cd: "CNTA" }, Id: "01690001041000579342" },
-                  },
-                  Purp: { Cd: "002" },
-                  RmtInf: { Ustrd: "DESCRIPCION DEL COBRO" },
-                },
-              },
-            ],
-          },
-        ],
-      },
-    },
-  };
-  */
+  console.log(`<<< ON emitSSEHandler (store)`, JSON.stringify(store));
 
   try {
     logger.info(store?.canNotify, "emit_sse_can_notify");
 
     if (store?.canNotify) {
-      logger.info("<<< CLOSE notifyHandler");
-
       const response = new Stream((stream) => {
         const interval = setInterval(() => {
           stream.send(JSON.stringify(store!.data));
@@ -168,8 +99,6 @@ export async function emitSSEHandler({ store, set }: Params) {
       });
 
       store.canNotify = false;
-
-      logger.info(store!.data, "notification_sse");
 
       return response;
     }
